1d089350bbc5df681544951a241454f17884b72d
Daffa Fawwaz Aditya <adityadaffafawwaz@gmail.com>
2023-06-09 19:28:58 +0700

    Fix UI Forum Chat Page

--- resources/js/pages/dashboard/ForumChat.jsx
+++ resources/js/pages/dashboard/ForumChat.jsx
@@ -7,31 +7,31 @@ import Avatar from "@mui/material/Avatar";
 import AvatarGroup from "@mui/material/AvatarGroup";
 export const ForumChat = () => {
   return (
-    <section className="mt-[calc(102px+20px)] md:mt-[calc(68px+20px)] flex flex-col w-full  h-full rounded-lg ">
+    <section className="mt-[calc(102px+20px)] md:mt-[calc(68px+20px)] flex flex-col w-full gap-5  h-full rounded-lg ">
       <h5 className="text-neutral-600 text-heading5">Data Kelompok</h5>
       <div className="flex flex-wrap gap-5">
-        {dataKelompok.map((value, index) => {
+        {dataKelompok.map(({ id, jmlPeserta, totalThreads, newThread, peserta }, index) => {
           return (
             <Link key={index} href="/forum-chat/kelompok" className="p-4 flex flex-col group bg-white shadow-defaultShadow rounded-lg gap-4 flex-[1_1_360px] transition-default hover:bg-skyBlue70 hover:text-white">
-              <h5 className="text-heading5 text-neutral-600 group-hover:text-inherit">Kelompok {value.id}</h5>
+              <h5 className="text-heading5 text-neutral-600 group-hover:text-inherit">Kelompok {id}</h5>
               <div className="flex flex-row text-neutral-500 gap-4 group-hover:text-inherit">
                 <div className="flex flex-col">
-                  <h2 className="text-heading2">{value.jmlPeserta}</h2>
+                  <h2 className="text-heading2">{jmlPeserta}</h2>
                   <p className="text-paragraph5">Jumlah Peserta</p>
                 </div>
                 <div className="flex flex-col">
-                  <h2 className="text-heading2">{value.totalThreads}</h2>
+                  <h2 className="text-heading2">{totalThreads}</h2>
                   <p className="text-paragraph5">Threads</p>
                 </div>
               </div>
               <div className="flex flex-col ">
                 <p className="text-paragraph3 text-neutral-500 group-hover:text-inherit">Threads terbaru</p>
-                <p className="text-paragraph4 text-neutral-400 leading-5 group-hover:text-inherit">{value.newThread}</p>
+                <p className="text-paragraph4 text-neutral-400 leading-5 group-hover:text-inherit">{newThread}</p>
               </div>
               <div className="flex flex-col items-start gap-2 ">
                 <p className="text-paragraph3 text-neutral-500 group-hover:text-inherit">Peserta</p>
                 <AvatarGroup max={5}>
-                  {value.peserta.map((name, index) => (
+                  {peserta.map((name, index) => (
                     <Avatar key={index} {...stringAvatar(name)} />
                   ))}
                 </AvatarGroup>
